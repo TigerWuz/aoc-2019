1e19e5acb1c73f4bbdc3334027b273f6b182ad1a
Piechocki Roland (CC-DA/EBR3) <roland.piechocki@de.bosch.com>
2019-12-09 14:42:22 +0100

    unefficient solution for day6 part 2

--- 06_day/06_day_part2.cpp
+++ 06_day/06_day_part2.cpp
@@ -1,46 +1,108 @@
 #include "inputParser.hpp"
 #include <map>
 #include <iostream>
+#include <queue>
+#include <list>
 
 using namespace std;
 
-// 1439 to low
+typedef struct Node {
+   string   name;
+   Node     *parent;
+   vector<struct Node *> children;
+};
+
+Node *newNode(string name) {
+   Node *n = new Node();
+   n->name = name;
+   return n;
+}
+
 
 int main() {
 
-   map<string, int> orbitalMap;
-   
-   vector<vector<string>> values;
-   inPa::inputParser::get("C:\\data\\aoc2019\\input\\day6.txt", values, ')');
+   vector<vector<string>> x;
+   inPa::inputParser::get("C:\\data\\aoc2019\\input\\day6.txt", x, ')');
 
+   list<vector<string>> values;
+   values.insert(values.begin(), x.begin(), x.end());
    //for this solution to work the "COM" mut be added into the map as first element
 
-   orbitalMap.insert(pair<string,int>(values[0][0], 0));
+
+
    //values.erase(values.begin());
+   Node *root = new Node();
+   root->name = values.begin()->at(0);
+   
+   queue <Node *> qu;
+   qu.push(root);
+   Node *current;
 
-   int i = 0;
-   while (values.size() != 0) {
-      auto center = orbitalMap.find(values[i][0]);
-      if (center == orbitalMap.end() ) {
-         // we do not know the root planet until now
-         //currently this is dangereous!
-         ++i;
-         continue;
+   Node *santa;
+   Node *you;
+
+   while (!qu.empty()) {
+      current = qu.front();
+      auto it = values.begin();
+      while (it != values.end()) {
+         if (it->at(0) == current->name) {
+            Node *child = new Node();
+            child->name = it->at(1);
+            child->parent = current;
+            current->children.push_back(child);
+            it = values.erase(it);
+            qu.push(child);
+
+            if (child->name == "YOU") {
+               you = child;
+            } else if (child->name == "SAN") {
+               santa = child;
+            }
+
+         } else {
+            ++it;
+         }
       }
-      orbitalMap.insert(pair<string,int>(values[i][1], center->second +1));
-      values.erase(values.begin() + i);
-      i = 0;
+      qu.pop();
    }
 
-   int orbitalSum = 0;
-   for (auto &p : orbitalMap) {
-      orbitalSum += p.second;
+   //my Path
+   Node *cP = you;
+   vector<string> myPath, santaPath;
+
+   while (cP->parent->name != "COM") {
+      myPath.push_back(cP->parent->name);
+      cP = cP->parent;
    }
 
-   // for (auto l : values) {
+   //santas Path
+   cP = santa;
+   while (cP->parent->name != "COM") {
+      santaPath.push_back(cP->parent->name);
+      cP = cP->parent;
+   }
 
-   // }
+   int myDistance = 0;
+   int santaDistance = 0;
+   bool exit = false;
 
-   cout << "Orbit Sum: "<<orbitalSum <<endl;
-   return 0;
+   for (auto mD: myPath) {
+      santaDistance = 0;
+      for (auto sD: santaPath) {
+         ++santaDistance;
+         if (mD == sD) {
+            //common point found
+            exit = true;
+            break;
+         }
+      }
+      ++myDistance;
+
+      if (exit == true) {
+         break;
+      }
+   }
+
+   int distance = myDistance + santaDistance -2;
+   cout <<"Distance to common point: " <<distance <<endl;
 }
